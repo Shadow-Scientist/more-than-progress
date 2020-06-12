f296a874de010e28ad9ab21b7b6bfc1b4b422198
Shadow Scientist <61514752+Shadow-Scientist@users.noreply.github.com>
2020-06-12 18:20:42 +0600

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,16 @@
+var progress_path, total_length;
+
+function init(){
+	progress_path = document.querySelector('.progress-container .progress');
+	total_length = progress_path.getTotalLength();
+	progress_path.style.setProperty('--total', total_length+'px');
+}
+
+function update(x)
+{
+	progress_path.style.strokeDashoffset = (total_length - (total_length*(x/100))) +'px'
+}
+
+window.addEventListener('load', function(){init()});
+
+/*js job done!*/
